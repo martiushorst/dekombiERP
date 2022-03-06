d57ab10027d35775ab4b524efa173c5e31b290ee
martiushorst <martius@graficagt.com.br>
2022-03-06 16:22:16 -0300

    Primeiro commit

--- cadastros/models.py
+++ cadastros/models.py
@@ -6,10 +6,10 @@ class Base(models.Model):
     criado = models.DateTimeField('Data de Criação', auto_now_add=True)
     modificado = models.DateTimeField('Data de Atualização', auto_now=True)
     ativo = models.BooleanField('Ativo?', default=True)
-
+    
     class Meta:
-        abstract = True #Rascunho para ser usado em outras classes, não é migrado
-        
+        abstract = True  # Rascunho para ser usado em outras classes, não é migrado
+
 
 class Cliente(Base):
     UF_CHOICES = (
@@ -41,112 +41,134 @@ class Cliente(Base):
         ('SE', 'Sergipe'),
         ('TO', 'Tocantins'),
     )
-    nome_razao_social = models.CharField('Nome/Razão Social', max_length=200)
-    celular = models.CharField('Celular', blank=False, max_length=15)
-    cpf_cnpj = models.CharField('CPF/CNPJ', blank=False, max_length=14)
-    rg_ie = models.CharField('RG/I.E.', blank=False, max_length=10)
-    endereco = models.CharField('Endereço', blank=False, max_length=150)
-    bairro = models.CharField('Bairro', blank=True, max_length=100)
-    numero = models.CharField('Nº', blank=True, max_length=10)
-    complemento = models.CharField('Complemento', blank=True, max_length=100)
-    cep = models.CharField('CEP', blank=False, max_length=9)
-    cidade = models.CharField(blank=False, max_length=50)
+    nome_razao_social = models.CharField('Nome/Razão Social', max_length=200, blank=False)
+    cpf_cnpj = models.CharField('CPF/CNPJ', max_length=14, blank=False)
+    rg_ie = models.CharField('RG/I.E.', max_length=10, blank=False)
+    endereco = models.CharField('Endereço', max_length=150, blank=False)
+    bairro = models.CharField('Bairro', max_length=100, blank=True)
+    numero = models.CharField('Nº', max_length=15, blank=True)
+    complemento = models.CharField('Complemento', max_length=100, blank=True)
+    cep = models.CharField('CEP', max_length=9, blank=False)
+    cidade = models.CharField('Cidade', max_length=50, blank=False)
     uf = models.CharField('UF', max_length=2, choices=UF_CHOICES, blank=False)
-
+    fone = models.CharField('Fone', max_length=10, blank=True)
+    celular = models.CharField('Celular', blank=False, max_length=15)
+    whats = models.BooleanField('Whats?', default=False)
+    email = models.EmailField('E-mail', max_length=254, blank=True)
+    data_nasc_abertura = models.DateField('data de nascimento/abertura', blank=True)
+    contato = models.CharField('Contato', max_length=50, blank=True)
+    
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
-        #ordering = ('created_at',)
-
+    
     def __str__(self):
-        return self.nome
-    
-    
-class Fornecedor(models.Model):
-    nome = models.CharField('Nome', max_length=200)
-    estado = models.CharField(
-        max_length=2,
-        default='SP',
-        choices=(
-            ('AC', 'Acre'),
-            ('AL', 'Alagoas'),
-            ('AP', 'Amapá'),
-            ('AM', 'Amazonas'),
-            ('BA', 'Bahia'),
-            ('CE', 'Ceará'),
-            ('DF', 'Distrito Federal'),
-            ('ES', 'Espírito Santo'),
-            ('GO', 'Goiás'),
-            ('MA', 'Maranhão'),
-            ('MT', 'Mato Grosso'),
-            ('MS', 'Mato Grosso do Sul'),
-            ('MG', 'Minas Gerais'),
-            ('PA', 'Pará'),
-            ('PB', 'Paraíba'),
-            ('PR', 'Paraná'),
-            ('PE', 'Pernambuco'),
-            ('PI', 'Piauí'),
-            ('RJ', 'Rio de Janeiro'),
-            ('RN', 'Rio Grande do Norte'),
-            ('RS', 'Rio Grande do Sul'),
-            ('RO', 'Rondônia'),
-            ('RR', 'Roraima'),
-            ('SC', 'Santa Catarina'),
-            ('SP', 'São Paulo'),
-            ('SE', 'Sergipe'),
-            ('TO', 'Tocantins'),
-        )
-    )
+        return self.nome_razao_social
 
+
+class Fornecedor(Base):
+    UF_CHOICES = (
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),
+    )
+    nome_razao_social = models.CharField('Nome/Razão Social', max_length=200, blank=False)
+    cpf_cnpj = models.CharField('CPF/CNPJ', max_length=14, blank=False)
+    rg_ie = models.CharField('RG/I.E.', max_length=10, blank=False)
+    endereco = models.CharField('Endereço', max_length=150, blank=False)
+    bairro = models.CharField('Bairro', max_length=100, blank=True)
+    numero = models.CharField('Nº', max_length=15, blank=True)
+    complemento = models.CharField('Complemento', max_length=100, blank=True)
+    cep = models.CharField('CEP', max_length=9, blank=False)
+    cidade = models.CharField('Cidade', max_length=50, blank=False)
+    uf = models.CharField('UF', max_length=2, choices=UF_CHOICES, blank=False)
+    fone = models.CharField('Fone', max_length=10, blank=True)
+    celular = models.CharField('Celular', blank=False, max_length=15)
+    whats = models.BooleanField('Whats?', default=False)
+    email = models.EmailField('E-mail', max_length=254, blank=True)
+    data_nasc_abertura = models.DateField('data de nascimento/abertura', blank=True)
+    contato = models.CharField('Contato', max_length=50, blank=True)
+    
     class Meta:
         verbose_name = 'Fornecedor'
         verbose_name_plural = 'Fornecedores'
-        #ordering = ('created_at',)
-
+    
     def __str__(self):
-        return self.nome
-    
-    
-class Funcionario(models.Model):
-    nome = models.CharField('Nome', max_length=200)
-    estado = models.CharField(
-        max_length=2,
-        default='SP',
-        choices=(
-            ('AC', 'Acre'),
-            ('AL', 'Alagoas'),
-            ('AP', 'Amapá'),
-            ('AM', 'Amazonas'),
-            ('BA', 'Bahia'),
-            ('CE', 'Ceará'),
-            ('DF', 'Distrito Federal'),
-            ('ES', 'Espírito Santo'),
-            ('GO', 'Goiás'),
-            ('MA', 'Maranhão'),
-            ('MT', 'Mato Grosso'),
-            ('MS', 'Mato Grosso do Sul'),
-            ('MG', 'Minas Gerais'),
-            ('PA', 'Pará'),
-            ('PB', 'Paraíba'),
-            ('PR', 'Paraná'),
-            ('PE', 'Pernambuco'),
-            ('PI', 'Piauí'),
-            ('RJ', 'Rio de Janeiro'),
-            ('RN', 'Rio Grande do Norte'),
-            ('RS', 'Rio Grande do Sul'),
-            ('RO', 'Rondônia'),
-            ('RR', 'Roraima'),
-            ('SC', 'Santa Catarina'),
-            ('SP', 'São Paulo'),
-            ('SE', 'Sergipe'),
-            ('TO', 'Tocantins'),
-        )
-    )
+        return self.nome_razao_social
 
+
+class Funcionario(Base):
+    UF_CHOICES = (
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),
+    )
+    nome = models.CharField('Nome/Razão Social', max_length=200, blank=False)
+    cpf = models.CharField('CPF/CNPJ', max_length=14, blank=False)
+    rg = models.CharField('RG/I.E.', max_length=10, blank=False)
+    endereco = models.CharField('Endereço', max_length=150, blank=False)
+    bairro = models.CharField('Bairro', max_length=100, blank=True)
+    numero = models.CharField('Nº', max_length=15, blank=True)
+    complemento = models.CharField('Complemento', max_length=100, blank=True)
+    cep = models.CharField('CEP', max_length=9, blank=False)
+    cidade = models.CharField('Cidade', max_length=50, blank=False)
+    uf = models.CharField('UF', max_length=2, choices=UF_CHOICES, blank=False)
+    fone = models.CharField('Fone', max_length=10, blank=True)
+    celular = models.CharField('Celular', blank=False, max_length=15)
+    whats = models.BooleanField('Whats?', default=False)
+    email = models.EmailField('E-mail', max_length=254, blank=True)
+    
     class Meta:
         verbose_name = 'Funcionário'
         verbose_name_plural = 'Funcionários'
-        #ordering = ('created_at',)
-
+    
     def __str__(self):
         return self.nome
--- erp/urls.py
+++ erp/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from erp import views
+from .views import MyPasswordSetView, MyPasswordChangeView
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('painel/', admin.site.urls),
-    path('dashboard/', include('cadastros.urls')),
+    path('admin/', admin.site.urls),
+
+    # Dashboards View
+    path('', views.DashboardView.as_view(), name='dashboard'),
+    
+    # Cadastros
+    #path('cadastros/', include('cadastros.urls')),
+    # Layouts
+    path('layout/', include('layout.urls')),
+    # Allauth
+    path('account/', include('allauth.urls')),
+    path('auth-logout/', TemplateView.as_view(template_name="account/logout-success.html"), name='pages-logout'),
+    path('auth-lockscreen/', TemplateView.as_view(template_name="account/lock-screen.html"), name='pages-lockscreen'),
+    #Custum change password done page redirect
+    path('accounts/password/change/', login_required(MyPasswordChangeView.as_view()), name="account_change_password"),
+    #Custum set password done page redirect
+    path('accounts/password/set/', login_required(MyPasswordSetView.as_view()), name="account_set_password"),
 ]
