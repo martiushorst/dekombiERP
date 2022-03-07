3817c13d636397e2eb32753667a1e865c2572b61
Martius Casa <martius@graficagt.com.br>
2022-03-06 23:42:40 -0300

    Primeiro commit

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    is_organisor = models.BooleanField(default=True)
+    is_agent = models.BooleanField(default=False)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Base(models.Model):
